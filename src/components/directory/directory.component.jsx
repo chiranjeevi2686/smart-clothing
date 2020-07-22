c8f7efc0057f40f3af767f107c38603ac08e2eaf
Chiru <kummu.infosys@gmail.com>
2020-07-22 16:23:12 +1000

    added route , shop and collection item components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,30 +11,35 @@ class Directory extends React.Component {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     size: '',
+                    linkUrl: 'hats',
                     id: 1
                 },
                 {
                     title: 'jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     size: '',
+                    linkUrl: 'jackets',
                     id: 2
                 },
                 {
                     title: 'sneakers',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                     size: '',
+                    linkUrl: 'sneakers',
                     id: 3
                 },
                 {
                     title: 'womens',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
+                    linkUrl: 'womens',
                     id: 4
                 },
                 {
                     title: 'mens',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
+                    linkUrl: 'mens',
                     id: 5
                 }
             ]
@@ -44,8 +49,8 @@ class Directory extends React.Component {
     render() {
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({ title, imageUrl, id, size }) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                {this.state.sections.map(({ id, ...otherSectionProps}) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))}
             </div>
         )
