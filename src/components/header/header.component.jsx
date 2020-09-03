6d5f59b13a8b9c5fbd09032dc9ad8332ba3b9e7d
Chiru <kummu.infosys@gmail.com>
2020-09-03 16:07:07 +1000

    implemented card reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import {connect} from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <div>
             <Link className='logo-container' to="/">
@@ -28,13 +28,14 @@ const Header = ({ currentUser }) => (
                     <Link className='option' to='/signin'> SIGN IN</Link>
             }
             <CartIcon/>
-            <CartDropdown/>
+      
         </div>
-       
+        {hidden ? null :<CartDropdown/>}
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) => ({
+    currentUser,
+    hidden
 })
 export default connect(mapStateToProps)(Header);
