73ebb820100420cc5758822cfb93ee4e8b6d6bea
Chiru <kummu.infosys@gmail.com>
2020-09-10 11:52:48 +1000

    added cart item component and used redux state for cart dropdown component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import  CollectionItem from '../collection-item/collection-item.component'
-import './collection-preview.styles.scss'
+import CollectionItem from '../collection-item/collection-item.component';
+import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ id, title, items }) => (
+const CollectionPreview = ({title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>
             {title.toUpperCase()}
@@ -10,8 +10,8 @@ const CollectionPreview = ({ id, title, items }) => (
         <div className='preview'>
             {items
                 .filter((item, idx) => idx < 4)
-                .map(({id, ...otherItemProps}) => (
-                    <CollectionItem key={id} {...otherItemProps} />
+                .map((item) => (
+                    <CollectionItem key={item.id} item={item} />
                 ))}
         </div>
 
