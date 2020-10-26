680b8e52ec74be706d0a76fd76898b25fe66ccfe
Chiru <kummu.infosys@gmail.com>
2020-10-26 17:49:48 +1100

    integrated stripe payments

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51HgOedKNue5JG4z7Tn3Qujei7mQ0IVokHUYKmFWBXoumql9tYoOMaZCj4f380QKArev81jMzgKHW2OQGZwwrTq4R00Gp1iVXM3';
+    const onToken = token => {
+        alert('Payment Successful');
+    }
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Smart Clothing'
+            billingAddress
+            shippingAddress
+            image=''
+            description={`your total is ${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+
+};
+export default StripeCheckoutButton;
