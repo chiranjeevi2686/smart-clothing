2af28202dc2f394c63ae3bf8cb04ef9fb7a02d4d
Chiru <kummu.infosys@gmail.com>
2020-12-08 14:53:43 +1100

    moved styles to jsx

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import CollectionItem from '../collection-item/collection-item.component';
-import './collection-preview.styles.scss';
+import { CollectionPreviewContainer, TitleContainer, PreviewContainer } from './collection-preview.styles.jsx';
 
-const CollectionPreview = ({title, items }) => (
-    <div className='collection-preview'>
-        <h1 className='title'>
+const CollectionPreview = ({ title, items }) => (
+    <CollectionPreviewContainer>
+        <TitleContainer>
             {title.toUpperCase()}
-        </h1>
-        <div className='preview'>
+        </TitleContainer>
+        <PreviewContainer>
             {items
                 .filter((item, idx) => idx < 4)
                 .map((item) => (
                     <CollectionItem key={item.id} item={item} />
                 ))}
-        </div>
+        </PreviewContainer>
 
-    </div>
+    </CollectionPreviewContainer>
 
 )
 export default CollectionPreview;
