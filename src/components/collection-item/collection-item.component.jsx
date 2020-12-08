6abcd28451b2813a9f470d5a3bb1e322b5f51eb7
Chiru <kummu.infosys@gmail.com>
2020-12-08 11:18:47 +1100

    moved styles to jsx

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './collection-item.styles.scss';
+import { CollectionItemContainer, BackgroundImage, CollectionFooterContainer, NameContainer, PriceContainer, AddButton } from './collection-item.styles.jsx';
 import CustomButton from '../custom-button/custom-button.component';
 import { connect } from 'react-redux';
 import { addItem } from '../../redux/cart/cart.actions';
@@ -10,21 +10,16 @@ const CollectionItem = ({ item, addItem }) => {
 
     const { imageUrl, name, price } = item;
     return (
-        <div className='collection-item'>
-            <div
-                className='image'
-                style={{
-                    background: `url(${imageUrl})`
-                }}
-            />
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
-            <CustomButton onClick={() => addItem(item)} inverted >
-                Add to cart
-            </CustomButton>
-        </div>
+        <CollectionItemContainer>
+            <BackgroundImage imageUrl={imageUrl} />
+            <CollectionFooterContainer>
+                <NameContainer>{name}</NameContainer>
+                <PriceContainer>${price}</PriceContainer>
+            </CollectionFooterContainer>
+            <AddButton onClick={() => addItem(item)} inverted >
+                ADD TO CART
+            </AddButton>
+        </CollectionItemContainer>
     )
 }
 
