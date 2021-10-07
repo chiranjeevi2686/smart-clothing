c72f386bab4c4dada1c18cf8ff54c8e09aac4dec
Chiru <kummu.infosys@gmail.com>
2021-10-07 14:27:25 +1100

    added express server

---
+++ client/build/precache-manifest.3290fec81c5a43d52e724c2000cfba9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8587b2da238abc9515e48d34b5b0b4e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40716a49265750aefcc4",
+    "url": "/static/css/main.3f1b7cd7.chunk.css"
+  },
+  {
+    "revision": "6c37e59f30b5f1829f64",
+    "url": "/static/js/2.b2e6225c.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.b2e6225c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40716a49265750aefcc4",
+    "url": "/static/js/main.f956d1c4.chunk.js"
+  },
+  {
+    "revision": "a5973a65ac54af68164a",
+    "url": "/static/js/runtime-main.f13fb989.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "5f8727c05ed1145471656c3f5672387e",
+    "url": "/static/media/smart.5f8727c0.svg"
+  }
+]);
--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51HgOedKNue5JG4z7Tn3Qujei7mQ0IVokHUYKmFWBXoumql9tYoOMaZCj4f380QKArev81jMzgKHW2OQGZwwrTq4R00Gp1iVXM3';
     const onToken = token => {
-        alert('Payment Successful');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token: token
+            }
+        }).then(response=>{
+            alert('Payment successful')
+        }).catch(error=>{
+            console.log('Payment error:',JSON.parse(error));
+            alert('Unfortunately payment was not successful, retry with correct payment details')
+        })
     }
     return (
         <StripeCheckout
